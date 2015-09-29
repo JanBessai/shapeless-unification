d6550dcffc08a3ae926efd7480fd5de38933ef44
Jan Bessai <Jan.Bessai@tu-dortmund.de>
2015-09-29 17:10:37 +0200

    Example with uncommented parts to demonstrate performance issues

--- examples/src/main/scala/shapeless/unification/examples/AST.scala
+++ examples/src/main/scala/shapeless/unification/examples/AST.scala
@@ -1,18 +1,10 @@
 package shapeless.unification.examples
 
+import shapeless.unification._
+
 object AST {
   sealed trait Exp
   case object NilExp extends Exp
-  case class BoolLit(b: Boolean) extends Exp
-  case class NumericLit(f: Double) extends Exp
-  case class StringLit(s: String) extends Exp
-  case object Dots extends Exp
-  /*case class FunctionDef(
-    params: ParListLike,
-    body: BlockLike
-  ) extends Exp
-  case class TableConstructor(fields: Seq[Field]) extends Exp with Args*/
-  //case class BinOpExp(op: BinOp, larg: Exp, rarg: Exp) extends Exp
   case class UnOpExp(op: UnOp, arg: Seq[Exp]) extends Exp
   
   sealed trait UnOp
@@ -20,4 +12,15 @@ object AST {
   case object BitNot extends UnOp
   case object Len extends UnOp
   
+  
+  /*case class BoolLit(b: Boolean) extends Exp
+  case class NumericLit(f: Double) extends Exp
+  case class StringLit(s: String) extends Exp
+  case object Dots extends Exp*/
+  
+  object SortedExample {
+    val sorted = Sorted[Exp]
+  }
+  
 }
+
